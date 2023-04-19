63057ba9c997ac8c00dc9c7195dc002bee4ca8e7
ColtonWright51 <cw717317@ohio.edu>
2023-04-19 14:56:46 -0400

    Plotted a 3D mesh in python from gmsh

--- src/colton.py
+++ src/colton.py
@@ -6,4 +6,19 @@ Scraps im working in to prevent merging problems
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
+columns = ('x', 'y', 'z')
+mesh = pd.read_csv("data\Feeder_Bracket_2.inp", skiprows=3, names=columns)
+print(mesh)
+mesh2 = mesh.to_numpy()
+
+x = mesh2[:, 0]
+y = mesh2[:, 1]
+z = mesh2[:, 2]
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(x,y,z)
+ax.axis('equal')
+plt.show()
