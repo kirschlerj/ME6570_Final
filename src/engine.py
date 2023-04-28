da8a9f889043e0b80d89c5d389a69c8ae14ba6ba
ColtonWright51 <cw717317@ohio.edu>
2023-04-28 15:59:56 -0400

    engine and main2 updates

--- src/engine.py
+++ src/engine.py
@@ -4,6 +4,9 @@ Engine for the main. Use FEM to approximate stress/strain in the solid.
 
 
 import numpy as np
+import sys
+np.set_printoptions(precision=3
+                    ,linewidth=sys.maxsize)
 
 
 class Load():
@@ -19,63 +22,85 @@ class Load():
 
 
 class Engine():
-    def __init__(self, nodes, tets, ElementType, GlobalMesh, DMatrix, YoungsModulus, PoissonsRatio):
+    # (self, nodes, tets, bricks=-1, ElementType, GlobalMesh, DMatrix, YoungsModulus, PoissonsRatio):
+    def __init__(self, nodes, tets, YoungsModulus, PoissonsRatio, bricks=-1):
         print("Initialize engine...")
-        self.init_material_properties()
+        self.init_material_properties(YoungsModulus, PoissonsRatio)
+        self.nodes = nodes # Node positions
+        self.tets = tets # The iconn, nodal tag of each element
+        self.bricks = bricks
+        self.num_nodes = np.shape(self.nodes)[0] # Get number of nodes in the mesh
+        self.num_elements = np.shape(self.tets)[0] # Get number of elements in the mesh
+        # self.R = self.get_R() # Needs work if we do bricks
+
+        self.K_global = np.zeros((self.num_nodes, self.num_nodes))
+        self.F = np.zeros(self.num_nodes)
+
 
     def solve(self):
         print("Solving...")
 
-    def init_Kelm(self, ElementType, GlobalMesh, DMatrix):
-        gm = GlobalMesh
-        numnode = np.size(gm)/3 # Calcs number of nodes from 3D global mesh
+        for i in range(self.num_elements):
+            Kelm = self.get_Kelm(i)
+
+
+    def get_Kelm(self, element_index):
+
+        # Take the mesh and get the nodes
+        node0x = self.nodes[self.tets[element_index, 0], 0]
+        node0y = self.nodes[self.tets[element_index, 0], 1]
+        node0z = self.nodes[self.tets[element_index, 0], 2]
+
+        node1x = self.nodes[self.tets[element_index, 1], 0]
+        node1y = self.nodes[self.tets[element_index, 1], 1]
+        node1z = self.nodes[self.tets[element_index, 1], 2]
+
+        node2x = self.nodes[self.tets[element_index, 2], 0]
+        node2y = self.nodes[self.tets[element_index, 2], 1]
+        node2z = self.nodes[self.tets[element_index, 2], 2]
+
+        node3x = self.nodes[self.tets[element_index, 3], 0]
+        node3y = self.nodes[self.tets[element_index, 3], 1]
+        node3z = self.nodes[self.tets[element_index, 3], 2]
+
+        gm = np.array([[node0x, node0y, node0z],
+                       [node1x, node1y, node1z],
+                       [node2x, node2y, node2z],
+                       [node3x, node3y, node3z]])
+        
+        print("self.tets[element_index]", self.tets[element_index])
+        print("node0x", node0x)
+        print("gm", gm)
+
         gaussint = np.array([(-1*np.sqrt(3/5)), 0, np.sqrt(3/5)]) # 3 point Gauss integration
         gaussweight = np.array([5/9, 8/9, 5/9]) # 3 point Gauss integration
-        self.bs = np.zeros((6, int(numnode*3)))
-        if ElementType.lower() == "brick": # Shape functions for brick elements
-            # Parent element labeled per notes
-            for i in range(3):
-                r = gaussint[i]
-                for j in range(3):
-                    s = gaussint[j]
-                    for k in range(3):
-                        t = gaussint[k]
-                        self.R = 0.125*np.array([[-1*(1-s)*(1-t), (1-s)*(1-t), (1+s)*(1-t), -(1+s)*(1-t), 
-                                                -(1-s)*(1+t), (1-s)*(1+t), (1+s)*(1+t), -(1+s)*(1+t)],
-                                                [-(1-r)*(1-t), -(1+r)*(1-t), (1+r)*(1-t), (1-r)*(1-t),
-                                                -(1-r)*(1+t), -(1+r)*(1+t), (1+r)*(1+t), (1-r)*(1+t)],
-                                                [-(1-r)*(1-s), -(1+r)*(1-s), -(1+r)*(1+s), -(1-r)*(1+s),
-                                                (1-r)*(1-s), (1+r)*(1-s), (1+r)*(1+s), (1-r)*(1+s)]])
-                        self.Jac = np.matmul(self.R,gm)
-                        dN = np.matmul(np.linalg.inv(self.Jac),self.R)
-                        for ii in range(8):
-                            icol = 3*ii
-                            self.bs[0:6,icol:icol+3] = np.array([[dN[0][ii], 0, 0],
-                                                                [0, dN[1][ii], 0],
-                                                                [0, 0, dN[2][ii]],
-                                                                [0, dN[2][ii], dN[1][ii]],
-                                                                [dN[2][ii], 0, dN[0][ii]],
-                                                                [dN[1][ii], dN[0][ii], 0]])
-        elif ElementType.lower() == "tet": # Shape functions for tet elements
-            # Parent element labeled per notesw
-            self.R = np.array([[1, 0, 0, -1],
+        Bs = np.zeros((6, 12)) # TODO: This is only goind to work for tets
+        R = self.get_R(element_index)
+        Jac = np.matmul(R,gm)
+        dN = np.matmul(np.linalg.inv(Jac), R)
+        for ii in range(4):
+            icol = 3*ii
+            Bs[0:6,icol:icol+3] = np.array([[dN[0,ii], 0, 0],
+                            [0, dN[1,ii], 0],
+                            [0, 0, dN[2,ii]],
+                            [0, dN[2,ii], dN[1,ii]],
+                            [dN[2,ii], 0, dN[0,ii]],
+                            [dN[1,ii], dN[0,ii], 0]])
+            print(Bs)
+        self.K = np.matmul(np.matmul(self.bs.transpose(), DMatrix), self.bs)*np.linalg.det(self.Jac)
+
+
+    def get_R(self, element_index):
+        # TODO: If we ever move up to bricks & tets in the same mesh, this needs some more work.
+        # We would need to recalculate this several times for each brick, determine if it's a
+        # brick or a tet.
+        
+        if self.bricks == -1:
+            # There are no bricks in our mesh, they were not assigned any value
+            R = np.array([[1, 0, 0, -1],
                           [0, 1, 0, -1],
                           [0, 0, 1, -1]])
-            self.Jac = np.matmul(self.R,gm)
-            dN = np.matmul(np.linalg.inv(self.Jac),self.R)
-            for ii in range(4):
-                icol = 3*ii
-                self.bs[0:6,icol:icol+3] = np.array([[dN[0,ii], 0, 0],
-                                [0, dN[1,ii], 0],
-                                [0, 0, dN[2,ii]],
-                                [0, dN[2,ii], dN[1,ii]],
-                                [dN[2,ii], 0, dN[0,ii]],
-                                [dN[1,ii], dN[0,ii], 0]])
-            self.K = np.matmul(np.matmul(self.bs.transpose(), DMatrix), self.bs)*np.linalg.det(self.Jac)
-        else:
-            print("Invalid Element type")
-            exit()
-
+        return R
 
 
     def init_material_properties(self, YoungsModulus, PoissonsRatio):
@@ -115,10 +140,17 @@ def main():
         print(in2.K)
 
 def main2():
-    pass
+    import input
+    full_path_to_stp = r"C:\Users\Colton W\Documents\GitHub\ME6570_Final\data\t20_data.step"
+
+    nodes, tets = input.stp_to_mesh(full_path_to_stp, show_gui=False)
+
+    engine = Engine(nodes, tets, YoungsModulus=196*10**11, PoissonsRatio=0.282)
+    engine.solve()
+    engine = Engine()
 
 
 if __name__ == '__main__':
-    main()
-    # main2()
+    # main()
+    main2()
 
