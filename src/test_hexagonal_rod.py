6f206d0ad148b67bbe679f5667c10435f824dfa2
ColtonWright51 <cw717317@ohio.edu>
2023-05-04 11:49:06 -0400

    commit

--- src/test_hexagonal_rod.py
+++ src/test_hexagonal_rod.py
@@ -1,7 +1,7 @@
 import engine
 import test_input
 import matplotlib.pyplot as plt
-import test_output
+import output
 import numpy as np
 import os
 import input
@@ -15,4 +15,4 @@ NBC = np.array([[4, 'z', 5000], [4, 'y', 5000], [4, 'x', 50000]])
 EBC = np.array([[1, 'z'], [1, 'x'], [1, 'y'], [2, 'z'], [2, 'x'], [2, 'y'], [3, 'z'], [3, 'x'], [3, 'y']])
 engine1 = engine.Engine(nodes = nodes, tets = tets, NBCs = NBC, EBCs = EBC, YoungsModulus= 1500, PoissonsRatio= 0.30)
 engine1.solve()
-test_output.plot_all(engine1)
+output.plot_all(engine1)
